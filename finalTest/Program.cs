e5c5b315df517e48b900907c53334730584d0810
maxim_stoma <maksim.stoma@mail.ru>
2022-07-05 00:31:08 +0300

    написана функция, которая находит элементы массива с длиной строки меньше или равной 3 символам

--- finalTest/Program.cs
+++ finalTest/Program.cs
@@ -22,3 +22,29 @@ void PrintArray(string[] stringArrayToPrint)
     Console.Write(" ]");
     Console.WriteLine();
 }
+string[] FindThreeSymbolElements(string[] threeSymbolElements)
+{
+    int count = 0;
+    string arrayElement = String.Empty;
+    for (int i = 0; i < threeSymbolElements.Length; i++)
+    {
+        arrayElement = threeSymbolElements[i];
+        if (arrayElement.Length < 4)
+        {
+            count++;
+        }
+    }
+    string[] result = new string[count];
+    
+    int calc = 0;
+    for (int i = 0; i < threeSymbolElements.Length; i++)
+    {
+        arrayElement = threeSymbolElements[i];
+        if (arrayElement.Length < 4)
+        {
+            result[calc] = arrayElement;
+            calc++;
+        }
+    }
+    return result;
+}
