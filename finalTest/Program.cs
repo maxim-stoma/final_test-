9d30b09ff36bd23180ef2cd47b43b031a67bf9fb
maxim_stoma <maksim.stoma@mail.ru>
2022-07-05 00:18:31 +0300

    написана функция, позволяющая заполнять массив произвольными строками с клавиатуры

--- finalTest/Program.cs
+++ finalTest/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void SetArray(string[] stringArray)
+{
+    Console.WriteLine("Напечатай выдуманные слова: ");
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+        Console.Write($"{i + 1}-е слово в твоём словаре: "); ;
+        stringArray[i] = Console.ReadLine()!;
+    }
+
+}
